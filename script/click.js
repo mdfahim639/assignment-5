4b79f6eb0a43ba3596fb77e0a3aad876c8ec8359
mdfahim639 <fffahim483@gmail.com>
2025-03-02 16:00:36 +0600

    first commit

---
+++ script/click.js
@@ -0,0 +1,31 @@
+document.getElementById('new-today').addEventListener('click', function () {
+  window.location.href = '/ans.html';
+});
+
+document.getElementById('btn-1').addEventListener('click', function () {
+  const taskElement = document.getElementById('task');
+  let value = parseInt(taskElement.innerText);
+
+  if (value > 0) {
+    value -= 1;
+    taskElement.innerText = value;
+
+    alert('Board Updated Successfully');
+
+    const btn = document.getElementById('btn-1');
+    btn.setAttribute('disabled', true);
+
+    if (value === 0) {
+      alert('yes');
+    }
+
+    const addHistory = document.getElementById('history');
+    const cardTitle = document.getElementById('card-title-1').innerText;
+
+    const historyItem = document.createElement('div');
+    historyItem.innerText = `Completed: ${cardTitle}`;
+    // appendChild.add = 'clear';
+    historyItem.classList.add('extra-style');
+    addHistory.appendChild(historyItem);
+  }
+});
